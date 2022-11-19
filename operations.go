9a0db4796a1dbaee141c9c45bf5468abea7147eb
Valentin <val.gougeon@hotmail.fr>
2022-11-19 19:01:34 +0100

    add reduce operation

--- operations.go
+++ operations.go
@@ -1,28 +1,38 @@
 package rhin
 
-func Filter[T any](condition func(item T) bool) func([]T) []T {
+func Filter[T any](condition func(T) bool) func([]T) []T {
 	return func(items []T) []T {
-		newItems := []T{}
+		result := []T{}
 		for i := 0; i < len(items); i++ {
 			if res := condition(items[i]); res {
-				newItems = append(newItems, items[i])
+				result = append(result, items[i])
 			}
 		}
-		return newItems
+		return result
 	}
 }
 
-func Map[T any, X any](transform func(item T) X) func([]T) []X {
+func Map[T any, X any](transform func(T) X) func([]T) []X {
 	return func(items []T) []X {
-		newItems := []X{}
+		result := []X{}
 		for i := 0; i < len(items); i++ {
-			newItems = append(newItems, transform(items[i]))
+			result = append(result, transform(items[i]))
 		}
-		return newItems
+		return result
 	}
 }
 
-func Every[T any](condition func(item T) bool) func([]T) bool {
+func Reduce[T any, X any](reducer func(X, T) X, defaultValue X) func([]T) X {
+	result := defaultValue
+	return func(items []T) X {
+		for i := 0; i < len(items); i++ {
+			result = reducer(result, items[i])
+		}
+		return result
+	}
+}
+
+func Every[T any](condition func(T) bool) func([]T) bool {
 	return func(items []T) bool {
 		for i := 0; i < len(items); i++ {
 			if res := condition(items[i]); !res {
